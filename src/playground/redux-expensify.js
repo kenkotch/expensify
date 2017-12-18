79e3d9b2c4310d46e8c46213d1c5423d9696a065
Ken Kotch <hello@ken.uno>
2017-12-17 23:27:28 -0700

    add ability to filter by text, startDate, endDate and sort by date or amount

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -32,10 +32,32 @@ const editExpense = (id, updates) => ({
 })
 
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+})
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT'
+})
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate
+})
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate
+})
 
 // expenses reducer
 const expensesReducerDefaultState = []
@@ -78,9 +100,52 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      }
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      }
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      }
     default:
       return state
   }
+}
+
+// timestamps start from Jan 1, 1970 @ midnight in milliseconds (unix epoch)
+// get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
+
+    return startDateMatch && endDateMatch && textMatch
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
+  })
 }
 
 // store creation
@@ -92,24 +157,26 @@ const store = createStore(
 )
 
 store.subscribe(() => {
-  console.log(store.getState())
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses)
 })
 
-const expenseOne = store.dispatch(addExpense({
-  description: 'Rent',
-  amount: 100
-}))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: 21000 }))
+//
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+//
+// store.dispatch(setTextFilter('ee'))
+// store.dispatch(setTextFilter())
+//
+// store.dispatch(sortByAmount())
+// store.dispatch(sortByDate())
 
-const expenseTwo = store.dispatch(addExpense({
-  description: 'Coffee',
-  amount: 300
-}))
-
-store.dispatch(removeExpense({
-  id: expenseOne.expense.id
-}))
-
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+// store.dispatch(setStartDate(0))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(999))
 
 const demoState = {
   expenses: [{
